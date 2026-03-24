566ca223fc7f6b46a04a908654084ff3c3d975b9
Jacob Rosario <jacobcvrosario@gmail.com>
2026-03-24 01:39:10 -0700

    finished net

--- src/components/Scene/createNet.js
+++ src/components/Scene/createNet.js
@@ -10,15 +10,15 @@ function createNet(scene) {
 
 
     // Left pole
-    const poleGeo = new THREE.CylinderGeometry(0.06, 0.06, netHeight + 0.04, 16)
+    const poleGeo = new THREE.CylinderGeometry(0.06, 0.06, 2.5, 16)
     const poleMat = new THREE.MeshPhongMaterial({ color: 0xff2200 })
     const leftPole = new THREE.Mesh(poleGeo, poleMat)
-    leftPole.position.set(-netWidth / 2, netY + netHeight / 2 + 0.2, netZ)
+    leftPole.position.set((-netWidth / 2), (netY / 2 + 0.2), netZ)
     netGroup.add(leftPole)
 
     // Right pole
     const rightPole = new THREE.Mesh(poleGeo, poleMat)
-    rightPole.position.set(netWidth / 2, netY + netHeight / 2 + 0.2, netZ)
+    rightPole.position.set((netWidth / 2), (netY / 2 + 0.2), netZ)
     netGroup.add(rightPole)
 
     // Tape
@@ -31,7 +31,27 @@ function createNet(scene) {
     // Net Strings
     const netMat = new THREE.LineBasicMaterial({ color: 0xffffff, opacity: 0.6, transparent: true })
 
+    // Vertical strings
+    for (let i = 1; i <= 11; i++) {
+        const x = ((i / 12) * netWidth) - (netWidth / 2)
+        const points = [
+            new THREE.Vector3(x, netY, netZ),
+            new THREE.Vector3(x, (netY + netHeight), netZ)
+        ]
+        const geo = new THREE.BufferGeometry().setFromPoints(points)
+        netGroup.add(new THREE.Line(geo, netMat))
+    }
 
+    // Horizontal strings
+    for (let i = 0; i <= 6; i++) {
+        const y = netY + ((i / 6) * netHeight)
+        const points = [
+            new THREE.Vector3((-netWidth / 2), y, netZ),
+            new THREE.Vector3((netWidth / 2), y, netZ)
+        ]
+        const geo = new THREE.BufferGeometry().setFromPoints(points)
+        netGroup.add(new THREE.Line(geo, netMat))
+    }
 
 
 
